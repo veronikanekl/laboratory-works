8617fcd4a8412fa507ac2f6cb8eae0ccd66c7031
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 14:59:42 +0600

    lab-5

---
+++ lab5/lab5/InsertIntroArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class InsertIntroArray
+    {
+        public List<int> Insert(List<int> array)
+        {
+            int sum = Math.Abs(array[0]) + Math.Abs(array[1]) + Math.Abs(array[array.Count - 1]);
+
+            for (int i = 0; i < array.Count; ++i)
+            {
+                if (sum % array[i] == 0)
+                {
+                    array.Insert(++i, 0);
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Исходный массив чисел: ");
+            List<int> array = new List<int> { 4, 5, 6, 7, -11, -3, 4, -5, -7, 2 };
+            Console.WriteLine(string.Join(" ", array));
+            Console.Write("Измененный массив:\n");
+
+            InsertIntroArray inserter = new InsertIntroArray();
+            List<int> result = inserter.Insert(array);
+            Console.WriteLine();
+
+            foreach (int element in result)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
