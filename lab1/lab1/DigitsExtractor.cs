0b6202a437bbe8c8849e7a9d9cbe59540f0f4362
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 14:23:21 +0600

    lab-1

---
+++ lab1/lab1/DigitsExtractor.cs
@@ -0,0 +1,17 @@
+﻿namespace lab1
+{
+    public class DigitsExtractor
+    {
+        public int[] ExctractTreeDigits(int number)
+        {
+            int[] result = new int[3];
+            for(int i =0; i < result.Length; i++)
+            {
+                result[i] = number % 10;
+                number /= 10;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите, пожалуйста, шестизначное число:");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            DigitsExtractor extractror = new DigitsExtractor();
+            int[] firstTreeDigits = extractror.ExctractTreeDigits(number);
+            int[] secondFirstTreeDigits = extractror.ExctractTreeDigits(number/1000);
+            int firstSum = firstTreeDigits[0] + firstTreeDigits[1] + firstTreeDigits[2];
+            int secondSum = secondFirstTreeDigits[0] + secondFirstTreeDigits[1] + secondFirstTreeDigits[2];
+            if(firstSum == secondSum)
+            {
+                Console.WriteLine("Поздравляем! Ваше число - счастливое :3");
+            }
+            else
+            {
+                Console.WriteLine("Извините, но ваше число - не счастливое :с");
+            }
+           
+            Console.ReadKey();  //аналог getch();
+        }
+    }
+}
