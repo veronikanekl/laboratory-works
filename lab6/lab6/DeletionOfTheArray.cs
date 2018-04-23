fffc6ff2b2e6a49f9eb0df5c918747fe88860b3f
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 15:00:54 +0600

    lab-6

---
+++ lab6/lab6/DeletionOfTheArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class DeletionOfTheArray
+    {
+        public List<int> Delete(List<int> array)
+        {
+            int temp = 0;
+            for (int j = 0; j < array.Count; j++)
+            {
+                temp += array[j];
+            }
+
+            int middle = temp / array.Count;
+
+
+            array.RemoveAll(i => i > middle);
+
+            return array;
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Исходный массив чисел: ");
+            List<int> array = new List<int> { 4, 5, 6, -4, -5, -12, -34, 3, 4 };
+            Console.WriteLine(string.Join(" ", array));
+
+            Console.Write("Измененный массив:\n");
+            DeletionOfTheArray operation = new DeletionOfTheArray();
+            List<int> result = operation.Delete(array);
+            Console.WriteLine(string.Join(" ", array));
+
+            Console.ReadKey();
+        }
+    }
+}
