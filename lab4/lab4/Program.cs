9bab18946a07926abc302dc2827404ff5e6f9bc6
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 14:58:05 +0600

    lab-4

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 5, 6, 7, -12, -3, 4, -5, -7, 2 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine(" ");
+            SwapMinimalNegativeAndMinimalPositive Count = new SwapMinimalNegativeAndMinimalPositive();
+            array = Count.Swap(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab4/lab4/SwapMinimalNegativeAndMinimalPositive.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class SwapMinimalNegativeAndMinimalPositive
+    {
+        public int[] Swap(int[] array)
+        {
+            int minimalNegative = int.MaxValue;
+            int minimalNegativeIndex = -1;
+            int minimalPositive = int.MaxValue;
+            int minimalPositiveIndex = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0 && array[i] < minimalNegative)
+                {
+                    minimalNegative = array[i];  //поиск максимального по модулю отрицательного  числа 
+                    minimalNegativeIndex = i;
+                }
+                else if (array[i] > 0 && array[i] < minimalPositive)
+                {
+                    minimalPositive = array[i];     //поиск максимального положительного числа
+                    minimalPositiveIndex = i;
+                }
+            }
+
+
+            if (minimalNegativeIndex != -1 && minimalPositiveIndex != -1)
+            {
+                int temp = array[minimalNegativeIndex];
+                array[minimalNegativeIndex] = array[minimalPositiveIndex];
+                array[minimalPositiveIndex] = temp;
+
+            }
+
+
+            return array;
+
+        }
+    }
+}
