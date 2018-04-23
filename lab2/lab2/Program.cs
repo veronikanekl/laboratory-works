843eced12ae4629a65ae0c4fc8d9d503f97aec37
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 14:32:26 +0600

    lab-2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Чем     примитивнее         человек, тем более   высокого        он о себе мнения.\n", temp;
+            Console.WriteLine("Изначальная строка:");
+            Console.WriteLine(str);
+            WordSeparation wordsSeparator = new WordSeparation();
+            string[] words = wordsSeparator.Separate(str);
+            str = words.Aggregate((result, word) => result + " " + word);
+            Console.WriteLine("Преобразованная строка:");
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2/lab2/WordSeparation.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace lab2
+{
+    class WordSeparation
+    {
+        public string[] Separate(string str)
+        {
+            string[] words = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+    }
+}
