17172b94076d4cad49e01a3a511cbe0f9ef998ff
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-04-23 15:01:10 +0600

    lab-7

---
+++ lab7/lab7/Permutation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Permutation
+    {
+        public (int, int) Approach(int[,] array)
+        {
+            int count = 0;
+            int sum = 0;
+            int middle = 0;
+            int arraySum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    arraySum += array[i, j];
+                }
+            }
+
+            middle = arraySum / (array.GetLength(0) * array.GetLength(1));
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i > j && array[i, j] > middle)
+                    {
+                        count++;
+                    }
+                    if (j >= array.GetLength(0) - i && array[i, j] > middle)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+
+            return (count, sum);
+        }
+    }
+}
---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[,] array = new int[n, n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(100);
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            Permutation Transfer = new Permutation();
+            (int count, int sum) = Transfer.Approach(array);
+            Console.WriteLine("\nКоличество элементов, больших среднего арифметического, находящихся над главной диагональю: {0}", count);
+            Console.WriteLine("\nСумма элементов, больших среднего арифметического, находящихся под главной диагональю: {0}", sum);
+
+            Console.Read();
+        }
+    }
+}
