3b009f89a7718e01cdd5818dc6dc247faad02048
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-05-28 22:01:19 +0600

    lab10

---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace lab10
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+        {
+            string line;
+           
+            StreamReader Reader = new StreamReader("...\\input.txt");
+            List<int> array = new List<int>();
+            while ((line = Reader.ReadLine()) != null)
+            {
+                array.Add(Convert.ToInt32(line));
+            }
+            foreach (int i in array)
+            {
+                Console.WriteLine(string.Join(" ", i));
+            }
+
+
+            Remover newArray = new Remover(array);
+            StreamWriter sw = new StreamWriter("...\\output.txt");
+            foreach (int i in newArray.Delete())
+            {
+                sw.Write(i + " ");
+            }
+            sw.Close();
+            Console.ReadLine();
+        }
+        }
+    }
+
---
+++ lab10/lab10/Remover.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab10
+{
+    class Remover
+    {
+        private List<int> array;
+        public Remover(List<int> beginArray)
+        {
+            array = beginArray;
+        }
+
+
+
+
+        public List<int> Delete()
+        {
+            int temp = 0;
+            for (int j = 0; j < array.Count; j++)
+            {
+                temp += array[j];
+            }
+
+            int middle = temp / array.Count;
+
+            array.RemoveAll(i => i > middle);
+
+            return array;
+        }
+    }
+}
