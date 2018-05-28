a3192db1086751b8ac9ea390da3534ce820e4733
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-05-28 22:02:05 +0600

    lab9

---
+++ lab9/lab9/ArraySorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab9
+{
+    class ArraySorting
+    {
+        public List<List<int>> Sorting(List<List<int>> array)
+        {
+            List<int> t = new List<int>();
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array.Count; j++) t.Add(array[i][j]);
+                t = t.OrderByDescending(x => x).ToList();
+                for (int j = 0; j < array.Count; j++) array[i][j] = t[j];
+                t.Clear();
+            }
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array.Count; j++) t.Add(array[j][i]);
+                t = t.OrderByDescending(x => x).ToList();
+                for (int j = 0; j < array.Count; j++) array[j][i] = t[j];
+                t.Clear();
+            }
+            return array;
+        }
+    }
+}
---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Упорядочить элементы в матрице по убыванию(слева направо,сверху вниз) \n");
+            int n = 4;
+            List<List<int>> array = new List<List<int>>();    //динамический двумерный массив
+            List<int> row = new List<int>();                //строка массива
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                row = new List<int>();
+                for (int j = 0; j < n; j++) row.Add(random.Next(-3, 7));
+                array.Add(row);                               //строка добавляется в массив
+            }
+
+            for (int i = 0; i < n; i++)                     //вывод массива
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(string.Format("{0,4}", array[i][j]));
+                }
+
+                Console.WriteLine();
+            }
+
+            ArraySorting method = new ArraySorting();
+            List<List<int>> result = method.Sorting(array);
+            Console.WriteLine();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(string.Format("{0,4}", result[i][j]));
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
