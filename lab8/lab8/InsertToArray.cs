33d91e3148ed974e3619e8643b948bfea9c1588b
veronikanekl <38648407+veronikanekl@users.noreply.github.com>
2018-05-28 22:01:39 +0600

    lab8

---
+++ lab8/lab8/InsertToArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab8
+{
+    class InsertToArray
+    {
+        public List<List<int>> Insert(List<List<int>> array)
+        {
+
+
+            List<int> summ = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array.Count; j++)
+                {
+                    if (array[i][j] < 0)
+                    {
+                        sum += array[i][j];
+                        // array[i].Insert(j++, summ[i]);
+                    }
+                }
+                summ.Add(sum);
+                sum = 0;
+                //Console.Write("{0}" + " ", summ[i]);
+            }
+            for (int i = 0; i < array.Count; i++)
+            {
+                array[i].Add(summ[i]);
+            }
+
+
+
+            return array;
+        }
+    }
+}
---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+
+            List<List<int>> array = new List<List<int>>();    //динамический двумерный массив
+            List<int> row = new List<int>();                //строка массива
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                row = new List<int>();
+                for (int j = 0; j < n; j++) row.Add(random.Next(-4, 7)); //строка массива заполняется просто суммой i и j
+                array.Add(row);                               //строка добавляется в массив
+            }
+
+            for (int i = 0; i < n; i++)                     //вывод массива
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(string.Format("{0,4}", array[i][j]));
+                }
+
+                Console.WriteLine();
+            }
+
+
+            InsertToArray method = new InsertToArray();
+            List<List<int>> result = method.Insert(array);
+            Console.WriteLine();
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int j = 0; j < result[i].Count; j++)
+                {
+                    Console.Write(string.Format("{0,4}", result[i][j]));
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
